036f92a7aee93e657499fdb9dd1c9d34a141df05
SHAIK ABDULHAMEED <99593105+AbdulhameedSk@users.noreply.github.com>
2023-08-04 08:32:53 +0530

    Time: 2398 ms (5.00%) | Memory: 44.9 MB (16.51%) - LeetSync

---
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.java
@@ -0,0 +1,23 @@
+import java.math.BigInteger;
+
+class Solution {
+    public String largestOddNumber(String num) {
+        BigInteger nums = new BigInteger(num);
+         if (num.length() == 0) {
+            return "";
+        }
+        if (nums.mod(BigInteger.valueOf(2)).equals(BigInteger.ONE)) {
+            return num;
+        } else {
+            String temp = "";
+    for (int i = num.length() - 1; i >= 0; i--) {
+        char samp = num.charAt(i);
+        if (Character.isDigit(samp) && Long.parseLong(String.valueOf(samp))%2 !=0) {
+            temp = num.substring(0, i + 1);
+            break;
+                }
+            }
+            return temp;
+        }
+    }
+}
