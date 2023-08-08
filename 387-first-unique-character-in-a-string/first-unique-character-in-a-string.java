42af51e2b72f19bbefd08e8aef553b5d90140cb0
SHAIK ABDULHAMEED <99593105+AbdulhameedSk@users.noreply.github.com>
2023-08-08 19:53:42 +0530

    Time: 31 ms (55.87%) | Memory: 44.4 MB (32.83%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -1,62 +1,16 @@
 class Solution {
     public int firstUniqChar(String s) {
-        int count=0;
-        int ans=0;
         Map<Character,Integer> map=new HashMap<>();
-        for(char c:s.toCharArray()){
-            if(map.containsKey(c)){
-                int value=map.get(c);
-                map.replace(c,value+1);
-            }
-            else{
-                map.put(c,1);
+        for(var ch:s.toCharArray()){
+            var count=map.containsKey(ch)?map.get(ch):0;
+            map.put(ch,count+1);
+        }
+       
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) == 1) {
+                return i;
             }
         }
-        System.out.println(map);
-            for(char i: s.toCharArray()){
-                count++;
-                if((map.get(i)).equals(1)){
-                    
-                    ans=map.get(i);
-                    System.out.println(ans);
-                    System.out.println(count);
-                    break;
-                }
-            }
-            if(s.length()==1){
-                return 0;
-            }
-            for(char f:map.keySet()){
-                if(map.get(f)==1){
-                    return count-1;
-                }
-
-            }
-            return -1;
-            // if(count==s.length()){
-            //     return -1;
-            // }
-            // return count-1;
-        
-    }  
-} 
-//         int i=0;
-//         Map<Integer,Character> map=new ConcurrentHashMap<>();
-//         for(char u:s.toCharArray()){
-//             map.put(i,u);
-//             i++;
-//         }
-// i=-1;
-// for (int j : map.keySet()) {
-//                 char v=map.get(j);
-//             i++;
-//             System.out.println(i);
-//              map.remove(j);
-//             if(!map.containsValue(v)){
-//                 return i;
-//             }
-//                       //  map.remove(j);
-
-//         }
-        
-// return -1;
+        return -1;
+    }
+}
