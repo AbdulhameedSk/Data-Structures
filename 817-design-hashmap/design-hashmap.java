1fb1a979b376a8f88988dab39c757e42f4de445e
SHAIK ABDULHAMEED <99593105+AbdulhameedSk@users.noreply.github.com>
2023-08-09 19:42:51 +0530

    Time: 22 ms (67.51%) | Memory: 49.8 MB (51.96%) - LeetSync

--- 817-design-hashmap/design-hashmap.java
+++ 817-design-hashmap/design-hashmap.java
@@ -1,21 +1,41 @@
-class MyHashMap {
-    int [] data = new int[1000001];
-    public MyHashMap() {
-        Arrays.fill(data,-1);
+ class Node{
+        int key;
+        int value;
+        Node(int key,int value){
+            this.key=key;
+            this.value=value;
+        }
     }
-    
+class MyHashMap {
+int ARRAY_SIZE = 10000;
+List<Node>[] data = new ArrayList[ARRAY_SIZE];
+
+    public MyHashMap() {
+        for(int i=0;i<ARRAY_SIZE;i++){
+            data[i]=new ArrayList<>();
+        }        
+    }
+    public int hash(int key){
+        return key%ARRAY_SIZE;
+    }
     public void put(int key, int value) {
-        data[key]=value;
+        int index=hash(key);
+        for(Node node:data[index]){
+        if(node.key==key){    node.value=value; return; }
+        }
+data[index].add(new Node(key,value));
     }
     
     public int get(int key) {
-        return data[key];
+        int index=hash(key);
+        for (Node node : data[index]) { if (node.key == key) { return node.value; }  }return -1;
     }
     
     public void remove(int key) {
-        data[key]=-1;
+        int index=hash(key);{
+        data[index].removeIf(node -> node.key==key);           
+        }
     }
-    
 }
 
 /**
