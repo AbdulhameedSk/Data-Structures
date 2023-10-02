fc924948be3d63b49573ddce8649c21816a12f89
SHAIK ABDULHAMEED <99593105+AbdulhameedSk@users.noreply.github.com>
2023-10-02 14:19:17 +0530

    Time: 31 ms (14.69%), Space: 56.8 MB (51.31%) - LeetHub

---
+++ 1966-frequency-of-the-most-frequent-element/1966-frequency-of-the-most-frequent-element.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxFrequency(int[] nums, int k) {
+        Arrays.sort(nums);
+        int r=0,l=0,ans=0;
+        long sum=0;
+        for(;r<nums.length;r++){
+            sum=sum+nums[r];
+            for(;nums[r]*(r-l+1)>sum+k;l++){
+                sum-=nums[l];
+            }
+            ans=Math.max(ans,r-l+1);
+        }
+        return ans;
+    }
+}
