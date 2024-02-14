6c69ea4a9b9e03df331d094176023567ea82706d
SHAIK ABDULHAMEED <99593105+AbdulhameedSk@users.noreply.github.com>
2024-02-14 10:28:04 +0530

    Time: 1 ms (100.00%) | Memory: 45.4 MB (95.00%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,17 +1,20 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        // snowball approach time=O(n) space=o(1)
-        int snowball=0;
-
+        int c=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]==0){
-                snowball++;
+                c++;
             }
-
-            else if(snowball>0){
-                nums[i-snowball]=nums[i];
-                nums[i]=0;
+        }
+        int l=0;
+        for(int r=0;r<nums.length;r++){
+            if(nums[r]!=0){
+                nums[l]=nums[r];
+                l++;
             }
+        }
+        for(int i=l;i<nums.length;i++){
+            nums[i]=0;
         }
     }
 }
