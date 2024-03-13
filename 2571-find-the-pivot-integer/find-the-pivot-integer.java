f7e1bfaa1e7aa811de86ac127dee24260f13d050
SHAIK ABDULHAMEED <99593105+AbdulhameedSk@users.noreply.github.com>
2024-03-13 07:09:53 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (33.98%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.java
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -1,19 +1,8 @@
 class Solution {
-    public static int sum(int a,int b){
-        int s=0;
-        for(int i=a;i<=b;i++){
-            s+=i;
-        }
-        return s;
-    }
     public int pivotInteger(int n) {
-        if(n==1){
-            return 1;
-        }
-        for(int i=1;i<n;i++){
-            if(sum(1,i)==sum(i,n)){
-                return i;
-            }
+        int p=(int)Math.sqrt(n*(n+1)/2);
+        if(p*p==n*(n+1)/2){
+            return p;
         }
         return -1;
     }
