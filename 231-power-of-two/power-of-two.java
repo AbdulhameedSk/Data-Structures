3f6d2ff23873a8debf3a8e74384ffc388188315c
SHAIK ABDULHAMEED <99593105+AbdulhameedSk@users.noreply.github.com>
2025-01-20 22:03:17 +0530

    Time: 1 ms (42.12%) | Memory: 40.7 MB (87.92%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,11 +1,19 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n<=0){
-            return false;
+        String bin=Integer.toBinaryString(n);
+        // if(bin.charAt(bin.length()-1)=='1'){
+        //     return false;
+        // }
+        if(n<=0)return false;
+        int c=0;
+        for(int i=bin.length()-1;i>=0;i--){
+            if(bin.charAt(i)=='1'){
+                c++;
+            }
+            if(c==2){
+                return false;
+            }
         }
-       if((n&(n-1)) ==0){
         return true;
-       } 
-       return false;
     }
 }
