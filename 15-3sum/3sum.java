f04efb0607068847827e126e444c14620522d67f
SHAIK ABDULHAMEED <99593105+AbdulhameedSk@users.noreply.github.com>
2026-01-21 00:00:13 +0530

    Time: 31 ms (79.37%) | Memory: 60.6 MB (5.39%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,30 +1,76 @@
+// class Solution {
+//     public List<List<Integer>> threeSum(int[] nums) {
+//         Arrays.sort(nums);
+
+//         List<List<Integer>> ans= new ArrayList<>();
+//         int len=nums.length;
+        
+//         for(int i=0;i<len-3;i++){
+//             if((i>0)&&(nums[i]==nums[i-1])  ){
+//                 continue;
+//             }
+//             int l=i+1;
+//             int r=len-1;
+//             int val=0-nums[i];
+//             while(l<r){
+//                 ArrayList<Integer> al=new ArrayList<>();
+//                 if(nums[l]+nums[r]==val){
+//                     al.add(nums[i]);
+//                     al.add(nums[l]);
+//                     al.add(nums[r]);
+//                     ans.add(al);
+//                     l++;
+//                     r--;
+//                 }else if(nums[l]+nums[r]<val){
+//                     l++;
+//                 }else{
+//                     r--;
+//                 }
+//             }
+//         }return ans;        
+//     }
+// }
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans=new ArrayList<>();
         Arrays.sort(nums);
+        List<List<Integer>> al=new ArrayList<>();
         for(int i=0;i<nums.length-2;i++){
-            if(i>0 && nums[i]==nums[i-1]){
-                continue;
+            if(i==0 || nums[i]!=nums[i-1]){
+                int a=nums[i];
+                TwoSum(nums,i+1,-a,al);
             }
-           int j=i+1;
-           int k=nums.length-1;
-           while(j<k){
-               int sum=nums[i]+nums[j]+nums[k];
-               if(sum==0){
-                   ans.add(Arrays.asList(nums[i],nums[j],nums[k]));
-               while(j<k && nums[j]==nums[j+1]){
-                   j++;
-               }
-               while(j<k && nums[k]==nums[k-1]){
-                   k--;
-               }
-               j++;
-               k--;
-           } else if(sum<0){
-               j++;
-           }else{
-               k--;
-           }}
-        }return ans;
-    }
+
+        }
+        return al;
+        }
+    
+    public static void TwoSum(int[] nums,int ind,int target,List<List<Integer>> al){
+        int i=ind;
+        int j=nums.length-1;
+        while(i<j){
+            int a=nums[i];
+            int b=nums[j];
+            if(a+b==target){
+                ArrayList<Integer> ans=new ArrayList<>();
+                ans.add(-target);
+                ans.add(a);
+                ans.add(b);
+                al.add(ans);
+                i++;
+            
+                while(i<j && nums[i]==nums[i-1]){
+                    i++;
+                }                    j--;
+
+                while(i<j && nums[j]==nums[j+1]){
+                    j--;
+                }
+            }else if(a+b<target){
+                i++;
+            }else{
+                j--;
+            }
+        }
+    }    
 }
